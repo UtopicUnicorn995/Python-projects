f3d14372527e00799b917065c0979e8e9c6e24f9
utopicUnicorn <christianbarrios995@gmail.com>
2023-07-27 01:26:25 +0800

    day-28: commit day-28 project

--- day-28/main.py
+++ day-28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -9,12 +10,65 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
+
+def reset_timer():
+    window.after_cancel(my_timer)
+    text.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    global reps
+    reps = 0
+    check_mark.config(text="")
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        text.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        text.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        text.config(text="Work!", fg=GREEN)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global my_timer
+        my_timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        if reps % 2 == 0:
+            check = math.floor(reps / 2) * "✔"
+            check_mark.config(text=f"{check}")
+        # Other answer
+        # checks = ""
+        # work_sessions = math.floor(reps/2)
+        # for _ in range(work_sessions):
+        #### checks += "✔"
+        #### check_mark.config(text=checks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -22,23 +76,28 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(
+    100, 135, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")
+)
+
+
 canvas.grid(column=1, row=1)
 
 text = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 52, "bold"))
 text.grid(column=1, row=0)
 
-start_button = Button(text="Start", width=12, highlightthickness=0)
+start_button = Button(text="Start", width=12, highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", width=12, highlightthickness=0)
+reset_button = Button(text="Reset", width=12, highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_mark = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
+check_mark = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
 check_mark.grid(column=1, row=3)
 
 window.mainloop()
