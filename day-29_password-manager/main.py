3af5697a712a97fc464ecf364c60f09bf77dbce7
utopicUnicorn <christianbarrios995@gmail.com>
2023-07-28 01:13:28 +0800

    day-29: password manager project

---
+++ day-29_password-manager/main.py
@@ -0,0 +1,40 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(height=200, width=200)
+img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=img)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2, sticky=(W, E))
+website_entry.focus
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2, sticky=(W, E))
+email_entry.insert(0, "Christian@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3, sticky=(W, E))
+
+
+password_button = Button(text="Generate Password")
+password_button.grid(column=2, row=3, sticky=(W, E))
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2, sticky=(W, E))
+
+window.mainloop()
