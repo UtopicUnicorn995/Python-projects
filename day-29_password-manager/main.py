69c1aa3b25f554ae80c43e6408b76572f6e722de
utopicUnicorn <christianbarrios995@gmail.com>
2023-07-29 23:35:48 +0800

    day-29: finished password manager

--- day-29_password-manager/main.py
+++ day-29_password-manager/main.py
@@ -1,21 +1,109 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+# Password Generator Project
+def generate_password():
+    letters = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+    password_list = []
+
+    [password_list.append(choice(letters)) for _ in range(randint(8, 10))]
+    [password_list.append(choice(symbols)) for _ in range(randint(2, 4))]
+    [password_list.append(choice(numbers)) for _ in range(randint(2, 4))]
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.delete(0, len(password))
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def save():
-    website = website_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
+# def save():
+#     website = website_entry.get()
+#     email = email_entry.get()
+#     password = password_entry.get()
 
-    messagebox.showinfo(title="Title", message="Adding complete")
-
-    f = open("data.txt", "a")
-    f.write(f"{website} || {email} || {password}\n")
-    f.close()
-    website_entry.delete(0, "end")
-    password_entry.delete(0, "end")
+#     if len(email) < 1 or len(password) < 1:
+#         messagebox.showwarning(
+#             title="Warning!", message="Please complete the all the fields"
+#         )
+#         return
+#     else:
+#         is_okay = messagebox.askokcancel(
+#             title=website,
+#             message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it okay to save?",
+#         )
+#         if is_okay:
+#             f = open("data.txt", "a")
+#             f.write(f"{website} || {email} || {password}\n")
+#             f.close()
+#             messagebox.showinfo(
+#                 title="Success!", message="You have successfully added a user!"
+#             )
+#     website_entry.delete(0, "end")
+#     password_entry.delete(0, "end")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -46,9 +134,9 @@ password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3, sticky=(W, E))
 
 
-password_button = Button(text="Generate Password")
+password_button = Button(text="Generate Password", command=generate_password)
 password_button.grid(column=2, row=3, sticky=(W, E))
-add_button = Button(text="Add", width=36, command=save)
+add_button = Button(text="Add", width=36)
 add_button.grid(column=1, row=4, columnspan=2, sticky=(W, E))
 
 window.mainloop()
