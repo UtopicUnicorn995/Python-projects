a21ce668e9dc7da1df6df9258ff2a29cd3ac3d74
utopicUnicorn <christianbarrios995@gmail.com>
2023-08-09 14:03:02 +0800

    day-34: finished creating the UI

--- day-34/ui.py
+++ day-34/ui.py
@@ -14,14 +14,14 @@ class QuizInterface:
         
         self.canvas = Canvas(height=250, width=300, bg='white')
         self.canvas.create_text(150, 125, text='ashdiasdhasd', fill=THEME_COLOR, font=('Arial 20 italic'))
-        self.canvas.grid(pady=20, column=0, row=1, columnspan=2)
+        self.canvas.grid(pady=(20, 50), column=0, row=1, columnspan=2)
         
-        check_btnImage = PhotoImage(file='images/true.png')
-        self.check_btn = Button(image=check_btnImage, highlightthickness=0)
+        true_img = PhotoImage(file='images/true.png')
+        self.check_btn = Button(image=true_img, highlightthickness=0)
         self.check_btn.grid(column=0, row=2)
         
-        x_btnImage = PhotoImage(file='images/false.png')
-        self.check_btn = Button(image=x_btnImage, highlightthickness=0)
+        false_img = PhotoImage(file='images/false.png')
+        self.check_btn = Button(image=false_img, highlightthickness=0)
         self.check_btn.grid(column=1, row=2)
         
         self.window.mainloop()
