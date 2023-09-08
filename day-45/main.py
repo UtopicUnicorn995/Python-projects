90bfbeea13a1c6c66dc806dcc62007c488c5fd29
utopicUnicorn <christianbarrios995@gmail.com>
2023-09-08 22:19:57 +0800

    day-45: added changes for day-45

--- day-45/main.py
+++ day-45/main.py
@@ -20,6 +20,33 @@ print(article_tag.string)
 # Results are the same
 
 article_link = article_tag.get("href")
-article_upvote = soup.find(name="span", id="score_37390184").string
+article_upvote = soup.find(name="span", class_="score").string
 print(article_link)
 print(article_upvote)
+
+articles = soup.select(".titleline a")
+
+article_text = []
+article_links = []
+article_upvotes_score = []
+article_upvotes = [score.string for score in soup.find_all(name="span", class_="score")]
+
+for article in articles:
+    article_links.append(article.get("href"))
+    article_text.append(article.getText())
+
+for upvote in article_upvotes:
+    article_upvotes_score.append(int(upvote.split(" ")[0]))
+
+
+print(article_upvotes_score)
+
+max = 0
+
+for num in range(len(article_upvotes_score)):
+    if article_upvotes_score[num] > max:
+        max = article_upvotes_score[num]
+        index = num
+
+print(article_text[index])
+print(article_links[index])
