909b1b268572b0e76a6e1a719d58cfd2ebe93558
utopicUnicorn <christianbarrios995@gmail.com>
2023-09-21 13:43:27 +0800

    day-46: created soup for top 100 songs of year 2000

--- day-46/main.py
+++ day-46/main.py
@@ -1,4 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy 
+from spotipy.oauth2 import SpotifyClientCredentials
 
-to_year = input('Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ')
+
+API_LINK = 'https://www.billboard.com/charts/hot-100/2000-08-12/'
+CLIENT_ID = '1bf8c7025249496c9fbd98f8e720937a'
+CLIENT_SECRET = 'e1eff8c05845485f8d1dacbf5d8b642a'
+# to_year = input('Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ')
+
+response = requests.get(API_LINK)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+titles = soup.find_all(name='h3', id='title-of-a-story', class_='a-no-trucate')
+
+# song_titles = []
+# for title in titles:
+#     song_titles.append(title.getText().strip())
+
+# print(song_titles)
+
+song_titles = [title.getText().strip() for title in titles]
+print(song_titles)
